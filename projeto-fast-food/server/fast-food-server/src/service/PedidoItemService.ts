ec2849f0e2051f406ebb0ef3cb6763f30501a471
eimmig <eduardo-immig@hotmai.com>
2023-11-26 18:45:11 -0300

    adicionado pedido item service

---
+++ projeto-fast-food/server/fast-food-server/src/service/PedidoItemService.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { GenericService } from './GenericService';
+import { PedidoItem } from 'src/model/pedido_item.model';
+
+@Injectable()
+export class PedidoItemService extends GenericService<PedidoItem> { 
+  constructor(
+    @InjectRepository(PedidoItem)
+    private readonly pedidoItemRepository: Repository<PedidoItem>,
+  ) {
+    super(PedidoItem, pedidoItemRepository)
+  }
+}
